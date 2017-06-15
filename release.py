bfb43a0147c09016f5290dd12e55544a2b4c33c0
David Vierra <codewarrior@hawaii.rr.com>
2017-06-14 15:48:15 -1000

    Deployment script

--- release.py
+++ release.py
@@ -11,12 +11,16 @@ ps_lupdate = path.join(ps_dir, "pyside-lupdate.exe")
 ps_lconvert = path.join(ps_dir, "lconvert.exe")
 ps_lrelease = path.join(ps_dir, "lrelease.exe")
 
-files = os.listdir("i18n")
+langdir = path.dirname(__file__)
+i18n = path.join(langdir, "i18n")
+build = path.join(langdir, "build")
+
+files = os.listdir(i18n)
 langs = [ts[:-3] for ts in files if ts.endswith(".ts")]
 
-if not path.exists("build"):
-    os.mkdir("build")
+if not path.exists(build):
+    os.mkdir(build)
 
 for lang in langs:
-    subprocess.check_call([ps_lrelease, "i18n/%s.ts" % lang, "-qm",
-                           "build/%s.qm" % lang])
+    subprocess.check_call([ps_lrelease, i18n + "/%s.ts" % lang, "-qm",
+                           build + "/%s.qm" % lang])
